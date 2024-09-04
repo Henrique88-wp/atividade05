e63ee723d657cc66dbb38eb3aed9180c5a0b7632
Henrique88-wp <phdsn411@gmail.com>
2024-09-04 16:10:24 -0300

    tarefa05 pro Sávio

---
+++ atividade05.py
@@ -0,0 +1,5 @@
+# Escreva um programa que receba um valor em minutos e converta para segundos.
+
+m = int(input("digite algum minuto"))
+s = m*60
+print(f"seu minuto escolhido é equivalente a {s} segundo.")
